91fba4898bb9ec6c81ae3c7a0c145d9997a8aef9
Sarah Jasim <115318360+SarahHJasim@users.noreply.github.com>
2023-02-06 15:28:32 -0500

    Project is set up with file header, and main().

---
+++ m1.cpp
@@ -0,0 +1,14 @@
+/*
+* Filename:		m1.cpp
+* Project:      Major Assignment 1
+* Programmer:	Sarah Jasim
+* Date:			2023-2-6
+* Description:  This program is meant to keep track of fares for flights and display information about them based on DS focused assignment 2.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	return 0;
+}
