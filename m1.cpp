cf3a752fdd323bf5930618c8bfd9e083504bb6a0
Sarah Jasim <115318360+SarahHJasim@users.noreply.github.com>
2023-02-16 12:56:56 -0500

    Modified fillFlightInfo function to best match a sorted double linked list.

--- m1.cpp
+++ m1.cpp
@@ -20,24 +20,25 @@
 //constants
 #define MAX_CHAR_LENG 29
 #define MIN_CHAR_LENG 1
+#define CHAR_LENG 30
 
 
 struct FlightNode
 {
-	struct FlightNode* head;
+	FlightNode* head;
 	struct FlightNode* prev;
-	char* sDestination;
-	char* sDate;
-	float* sFlightFare;
+	char* destination;
+	char* date;
+	double fare;
 	struct FlightNode* next;
-	struct FlightNode* tail;
+	FlightNode* tail;
 };
 
 
 //prototypes
 void fillFlightInfo(struct FlightNode** head, char* destination, char* date);
 void printFlightInfo(struct FlightNode* head);
-struct FlightNode* newAllocNode(char* destination, char* date);
+struct FlightNode* newAllocNode(char* destination, char* date, double fare);
 struct FlightNode* findFlight(FlightNode* head, char* destination, char* date);
 void deleteNode(FlightNode* node, FlightNode** head, FlightNode** tail);
 
@@ -52,6 +53,120 @@ int main()
 
 	return 0;
 
+}
+
+
+
+
+
+/*
+* Function:			fillFlightInfo()
+* Parameters:		struct FlightNode** head, char* destination, char* date
+* Return values:	None
+* Description:		This function adds a new flight to the list of flights by
+*					creating a new FlightNode structure using newAllocNode
+*					function, updating the head pointer to point to the newly
+*					created node, and updating the next field of the previous
+*					head node to point to the newly created node.
+*/
+
+void fillFlightInfo(struct FlightNode** head, char* sDestination, char* sDate, double sFlightFare)
+{
+	struct FlightNode* newNode = newAllocNode(sDestination, sDate, sFlightFare);
+	if (!newNode)
+	{
+		printf("Error: memory allocation failed\n");
+		return;
+	}
+
+	if (*head == NULL)
+	{
+		*head = newNode;
+		return;
+	}
+
+	struct FlightNode* curr = *head;
+	while (curr->next != NULL && strcmp(curr->next->date, sDate) < 0)
+	{
+		curr = curr->next;
+	}
+	while (curr->next != NULL && strcmp(curr->next->date, sDate) == 0 && strcmp(curr->next->destination, sDestination) < 0)
+	{
+		curr = curr->next;
+	}
+	while (curr->next != NULL && strcmp(curr->next->date, sDate) == 0 && strcmp(curr->next->destination, sDestination) == 0 && curr->next->flightFare < sFlightFare)
+	{
+		curr = curr->next;
+	}
+
+	newNode->next = curr->next;
+	curr->next = newNode;
+}
+
+
+
+
+
+
+/*
+* Function:			printFlightInfo()
+* Parameters:		struct FlightNode* head
+* Return values:	None
+* Description:		This function prints all the flights in the list by
+*					traversing the linked list starting from the head node
+*					and printing the destination, date, and fare fields of each
+*					FlightNode structure in the list.
+*/
+
+void printFlightInfo(struct FlightNode* head)
+{
+	while (head != NULL)
+	{
+		//printig the destination, date, and fare of the flight with a width of 35 characters and left-justified
+		printf("%-35s\n", head->destination);
+		printf("%-35s\n", head->date);
+		printf("%-.2f\n", head->fare);
+
+		head = head->next;
+	}
+}
+
+
+
+
+/*
+* Function:			FlightNode newAllocNode()
+* Parameters :		char* destination, char* date
+* Return values :	None
+* Description :		This function creates a new FlightNode structure, allocates
+*					memory for its destination and date fields, copies the values of
+*					destination and date into these fields, and sets the next field to
+*					NULL. It returns a pointer to the newly created FlightNode structure.
+
+*/
+
+struct FlightNode* newAllocNode(char* destination, char* date, double fare)
+{
+	struct FlightNode* newNode = (struct FlightNode*)malloc(sizeof(struct FlightNode));
+	newNode->destination = (char*)malloc(CHAR_LENG);
+	if (newNode->destination == NULL)
+	{
+		printf("Memory allocation failed.\n");
+		return 0;
+	}
+
+	newNode->date = (char*)malloc(CHAR_LENG);
+	if (newNode->date == NULL)
+	{
+		printf("Memory allocation failed.\n");
+		return 0;
+	}
+
+	strcpy(newNode->destination, destination);
+	strcpy(newNode->date, date);
+	newNode->next = NULL;
+
+	return newNode;
 }
 
 
@@ -79,7 +194,7 @@ struct FlightNode* findFlight(FlightNode* head, char* destination, char* date)
 	{
 		//it returns a pointer to the node containing a flight,
 		//if both the destination and date are matched
-		if (strcmp(current->sDestination, destination) == 0 && strcmp(current->sDate, date) == 0)
+		if (strcmp(current->destination, destination) == 0 && strcmp(current->date, date) == 0)
 		{
 			return current;
 		}
